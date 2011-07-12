83c455416a246f84d1ed254bdc19e4df69aaca96
Ben Weaver <ben@orangesoda.net>
2011-07-11 23:45:13 -0400

    Add controls for rotation and skewing; needs math work.

--- jquery.transform-ui.js
+++ jquery.transform-ui.js
@@ -53,15 +53,26 @@
        this._mouseDestroy();
      },
 
+     setMode: function(mode) {
+       this.mode = mode;
+     },
+
      _mouseCapture: function(ev) {
+       this.axis = null;
+
+       if (!(this.mode in this.change))
+         return false;
+
        for (var axis in this.handles) {
          if (this.handles[axis][0] == ev.target) {
-           this.axis = axis;
-           return true;
+           if (axis in this.change[this.mode]) {
+             this.axis = axis;
+             return true;
+           }
+           return false;
          }
        }
 
-       this.axis = null;
        return false;
      },
 
@@ -116,6 +127,83 @@
 
          w: function(ev, dx, dy) {
            return { left: this.origPos.left + dx, width: this.origSize.width - dx };
+         },
+
+         ne: function(ev, dx, dy) {
+           var base = this.change[this.mode];
+           return $.extend(base.n.call(this, ev, dx, dy), base.e.call(this, ev, dx, dy));
+         },
+
+         se: function(ev, dx, dy) {
+           var base = this.change[this.mode];
+           return $.extend(base.s.call(this, ev, dx, dy), base.e.call(this, ev, dx, dy));
+         },
+
+         sw: function(ev, dx, dy) {
+           var base = this.change[this.mode];
+           return $.extend(base.s.call(this, ev, dx, dy), base.w.call(this, ev, dx, dy));
+         },
+
+         nw: function(ev, dx, dy) {
+           var base = this.change[this.mode];
+           return $.extend(base.n.call(this, ev, dx, dy), base.w.call(this, ev, dx, dy));
+         }
+       },
+
+       rotate: {
+         start: function() {
+           this.origRotate = parseInt(this.element.css('rotate') || 0);
+         },
+
+         drag: function(ev, dx, dy, data) {
+           console.log('drag!', dx, dy, data);
+           this.element.transform({ rotate: (data % 360) + 'deg' });
+         },
+
+         ne: function(ev, dx, dy) {
+           return this.origRotate + dx;
+         },
+
+         se: function(ev, dx, dy) {
+           return this.origRotate - dx;
+         },
+
+         sw: function(ev, dx, dy) {
+           return this.origRotate - dx;
+         },
+
+         nw: function(ev, dx, dy) {
+           return this.origRotate + dx;
+         }
+       },
+
+       skew: {
+         start: function() {
+           this.origSkewX = parseInt(this.element.css('skewX') || 0);
+           this.origSkewY = parseInt(this.element.css('skewY') || 0);
+         },
+
+         drag: function(ev, dx, dy, data) {
+           this.element.transform({
+             skewX: this.origSkewX + (data.x || 0) + 'deg',
+             skewY: this.origSkewY + (data.y || 0) + 'deg'
+           });
+         },
+
+         n: function(ev, dx, dy) {
+           return { y: dy };
+         },
+
+         e: function(ev, dx, dy) {
+           return { x: dx };
+         },
+
+         s: function(ev, dx, dy) {
+           return { y: -1 * dy };
+         },
+
+         w: function(ev, dx, dy) {
+           return { x: -1 * dx };
          },
 
          ne: function(ev, dx, dy) {
